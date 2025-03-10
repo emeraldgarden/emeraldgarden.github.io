673248ba3b4e19cf01e617135940d6610562399c
Md Jahid Shah <55074901+MdJahidShah@users.noreply.github.com>
2025-03-10 17:29:37 +0600

    update

--- footer-script.js
+++ footer-script.js
@@ -1,38 +1,38 @@
 // Function to load navbar and footer
 document.addEventListener("DOMContentLoaded", function () {
-fetch("navbar.html")
-.then(response => response.text())
-.then(data => {
-document.getElementById("navbar-container").innerHTML = data;
-})
-.catch(error => console.error("Error loading the navbar:", error));
+    fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar-container").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading the navbar:", error));
+    
+    fetch("page-navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar-container-page").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading the navbar:", error));
+    
+    fetch("related-post.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("relatedpost-container-page").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading the navbar:", error));
 
-fetch("page-navbar.html")
-.then(response => response.text())
-.then(data => {
-document.getElementById("navbar-container-page").innerHTML = data;
-})
-.catch(error => console.error("Error loading the navbar:", error));
+    
+    fetch("sidebar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("sidebar-container-page").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading the navbar:", error));
 
-fetch("related-post.html")
-.then(response => response.text())
-.then(data => {
-document.getElementById("relatedpost-container-page").innerHTML = data;
-})
-.catch(error => console.error("Error loading the navbar:", error));
-
-
-fetch("sidebar.html")
-.then(response => response.text())
-.then(data => {
-document.getElementById("sidebar-container-page").innerHTML = data;
-})
-.catch(error => console.error("Error loading the navbar:", error));
-
-fetch("footer.html")
-.then(response => response.text())
-.then(data => {
-document.getElementById("footer-container").innerHTML = data;
-})
-.catch(error => console.error("Error loading the footer:", error));
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer-container").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading the footer:", error));
 });
